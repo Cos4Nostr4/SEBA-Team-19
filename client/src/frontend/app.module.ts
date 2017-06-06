bf2910f6832eb91f24aeda8c8b2b75666c7795e3
Alexander Castendyck <alexander.castendyck@gmx.de>
2017-06-06 15:59:05 +0200

    first module

--- client/src/frontend/app.module.ts
+++ client/src/frontend/app.module.ts
@@ -1,10 +1,17 @@
-import { NgModule }      from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent }  from './app.component';
+import {NgModule}      from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {AppComponent}  from './app.component';
+import {AppHeaderModule} from './app-header/app-header.module'
 
 @NgModule({
-    imports:      [ BrowserModule ],
-    declarations: [ AppComponent ],
-    bootstrap:    [ AppComponent ]
+    imports: [
+        BrowserModule,
+        AppHeaderModule
+    ],
+    declarations: [
+        AppComponent,
+    ],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
