d9cb0ccc5618bd45457d46264b211ec8b4ab5f52
Alexander Castendyck <alexander.castendyck@gmx.de>
2017-06-18 15:49:37 +0200

    make menu slider adaptable to different number of items shown

--- client/src/frontend/menu-slider/menu-slider.component.ts
+++ client/src/frontend/menu-slider/menu-slider.component.ts
@@ -1,11 +1,11 @@
-import {Component} from '@angular/core'
-declare var jquery:any;
-declare var $ :any;
+import {Component} from "@angular/core";
+declare var jquery: any;
+declare var $: any;
 
 @Component({
     selector: 'menu-slider',
     templateUrl: './menu-slider.component.html',
-    styleUrls: [ './menu-slider.component.css' ]
+    styleUrls: ['./menu-slider.component.css']
 })
 
 export class MenuSliderComponent {
@@ -13,17 +13,40 @@ export class MenuSliderComponent {
     ngOnInit(): void {
         "use strict";
 
-        let numberOfEelements = $('.col').length;
-        let indexOfLastElement = numberOfEelements -1;
+        const numberOfElementsShown = 8;
+        const waitingElements:any = [];
 
-        $('#next').click(function(){
-            let firstElement = $('.col').eq(0).detach();
-            $('#slider').append(firstElement);
+        let numberOfEelements = $('.menu-item').length;
+        let indexOfLastElement = numberOfEelements - 1;
+
+        for(let i =numberOfElementsShown; i<numberOfEelements; i++){
+            let waitingElement = $('.menu-item').eq(numberOfElementsShown);
+            waitingElement.detach();
+            waitingElements.push(waitingElement);
+            console.log("Removed element. Remaining: "+$('.menu-item').length +"; Waiting: "+waitingElements.length);
+        }
+
+        $('#next').click(function () {
+            console.log("Direction left.Remaining: "+$('.menu-item').length +"; Waiting: "+waitingElements.length);
+
+            $('.active-menu').removeClass('active-menu');
+            let firstElement = $('.menu-item').eq(0).detach();
+            waitingElements.unshift(firstElement);
+
+            let elementToAddBehind = waitingElements.pop();
+            $('#slider').append(elementToAddBehind);
+            $('.menu-item').eq(0).addClass('active-menu');
         });
 
-        $('#last').click(function(){
-            let firstElement = $('.col').eq(indexOfLastElement).detach();
-            $('#slider').prepend(firstElement);
+        $('#last').click(function () {
+            console.log("Direction right.Remaining: "+$('.menu-item').length +"; Waiting: "+waitingElements.length);
+            $('.active-menu').removeClass('active-menu');
+            let lastElement = $('.menu-item').last().detach();
+            waitingElements.push(lastElement);
+
+            let elementToAddInFront = waitingElements.shift();
+            $('#slider').prepend(elementToAddInFront);
+            $('.menu-item').eq(0).addClass('active-menu');
         });
     }
 
