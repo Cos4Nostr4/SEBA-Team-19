d2fac18d09fdf50a90bace47dfeb9f094403b954
Alexander Castendyck <alexander.castendyck@gmx.de>
2017-06-18 16:21:33 +0200

    make menu slider resizable

--- client/src/frontend/menu-slider/menu-slider.component.ts
+++ client/src/frontend/menu-slider/menu-slider.component.ts
@@ -13,41 +13,77 @@ export class MenuSliderComponent {
     ngOnInit(): void {
         "use strict";
 
-        const numberOfElementsShown = 8;
-        const waitingElements:any = [];
+        const numberOfElementsShownInitially: number = 8;
+        const waitingElements: any = [];
+        const minimumWidhtOfElementsInPixel: number = 200;
 
         let numberOfEelements = $('.menu-item').length;
-        let indexOfLastElement = numberOfEelements - 1;
 
-        for(let i =numberOfElementsShown; i<numberOfEelements; i++){
-            let waitingElement = $('.menu-item').eq(numberOfElementsShown);
-            waitingElement.detach();
-            waitingElements.push(waitingElement);
-            console.log("Removed element. Remaining: "+$('.menu-item').length +"; Waiting: "+waitingElements.length);
-        }
+        removeNotShownElements(numberOfEelements - numberOfElementsShownInitially);
 
         $('#next').click(function () {
-            console.log("Direction left.Remaining: "+$('.menu-item').length +"; Waiting: "+waitingElements.length);
+            console.log("Direction left.Remaining: " + $('.menu-item').length + "; Waiting: " + waitingElements.length);
 
             $('.active-menu').removeClass('active-menu');
-            let firstElement = $('.menu-item').eq(0).detach();
-            waitingElements.unshift(firstElement);
-
-            let elementToAddBehind = waitingElements.pop();
-            $('#slider').append(elementToAddBehind);
+            slideLeft();
             $('.menu-item').eq(0).addClass('active-menu');
         });
 
         $('#last').click(function () {
-            console.log("Direction right.Remaining: "+$('.menu-item').length +"; Waiting: "+waitingElements.length);
+            console.log("Direction right.Remaining: " + $('.menu-item').length + "; Waiting: " + waitingElements.length);
             $('.active-menu').removeClass('active-menu');
+            slideRight();
+            $('.menu-item').eq(0).addClass('active-menu');
+        });
+
+        $(window).bind('resize', function (e: any) {
+            let menuSliderWidth = $('#menu-slider-container').width();
+            console.log("Div resized to width of :" + menuSliderWidth);
+
+            let numberOfElementsToShow = Math.floor(menuSliderWidth / minimumWidhtOfElementsInPixel);
+
+            let numbeOfItems = $('.menu-item').length;
+            if(numberOfElementsToShow < numbeOfItems) {
+                let numberOfElementsToRemove = numbeOfItems - numberOfElementsToShow;
+                removeNotShownElements(numberOfElementsToRemove);
+            }
+
+            if(numberOfElementsToShow > numbeOfItems) {
+                addElementsToShow(numberOfElementsToShow - numbeOfItems);
+            }
+        });
+
+        function removeNotShownElements(numberOfElements: number) {
+            for (let i = 0; i < numberOfElements; i++) {
+                let waitingElement = $('.menu-item').last();
+                waitingElement.detach();
+                waitingElements.push(waitingElement);
+                console.log("Removed element. Remaining: " + $('.menu-item').length + "; Waiting: " + waitingElements.length);
+            }
+        }
+
+        function addElementsToShow(numberOfElementsToAdd: number) {
+            for(let i = 0; i < numberOfElementsToAdd; i++){
+                let elementToAddBehind = waitingElements.pop();
+                $('#slider').append(elementToAddBehind);
+            }
+        }
+
+        function slideRight() {
             let lastElement = $('.menu-item').last().detach();
             waitingElements.push(lastElement);
 
             let elementToAddInFront = waitingElements.shift();
             $('#slider').prepend(elementToAddInFront);
-            $('.menu-item').eq(0).addClass('active-menu');
-        });
+        }
+
+        function slideLeft() {
+            let firstElement = $('.menu-item').eq(0).detach();
+            waitingElements.unshift(firstElement);
+
+            let elementToAddBehind = waitingElements.pop();
+            $('#slider').append(elementToAddBehind);
+        }
     }
 
 }
