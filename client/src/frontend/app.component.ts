d2fbceaf18cdfdf1db199f48dff163ae2afbc456
Alexander Castendyck <alexander.castendyck@gmx.de>
2017-06-18 19:21:10 +0200

    first version final layout

--- client/src/frontend/app.component.ts
+++ client/src/frontend/app.component.ts
@@ -1,8 +1,11 @@
-import { Component } from '@angular/core';
+import {Component} from "@angular/core";
 
 @Component({
     selector: 'my-app',
-    template: `<app-header></app-header><menu-slider></menu-slider>
-    <router-outlet></router-outlet>`
+    template: `
+        <app-header></app-header>
+        <menu-slider></menu-slider>
+        <router-outlet></router-outlet>`
 })
-export class AppComponent {}
+export class AppComponent {
+}
