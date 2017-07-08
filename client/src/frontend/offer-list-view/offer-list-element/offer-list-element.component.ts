3be01405d101f8d027c933439ddc8d4f8babb7c6
jensKlinker <jens.klinker@peyclon.de>
2017-07-08 21:31:35 +0200

    Implement working list-element and link to corresponding detail page

--- client/src/frontend/offer-list-view/offer-list-element/offer-list-element.component.ts
+++ client/src/frontend/offer-list-view/offer-list-element/offer-list-element.component.ts
@@ -1,6 +1,7 @@
 import {Component, Input, OnInit} from "@angular/core";
 import {Campaign} from "../../data-objects/campaign";
 import {ImageService} from "../../services/image.service";
+import {Router} from "@angular/router";
 
 @Component({
     selector: 'offer-list-element',
@@ -13,16 +14,37 @@ export class OfferListElementComponent implements OnInit{
     @Input() offer: Campaign;
     private imageSrc: string;
     private imageService: ImageService;
+    private router: Router;
 
-    constructor(imageService: ImageService) {
+    constructor(imageService: ImageService, router: Router) {
         this.imageService = imageService;
-
+        this.router = router;
     }
 
     ngOnInit(): void {
         let imageName = this.offer.image;
-        let imageUrl = this.imageService.getOfferPreviewUrlFor( imageName);
+        let imageUrl = this.imageService.getOfferPreviewUrlFor(imageName);
         this.imageSrc= imageUrl;
+    }
+
+    myFunction() {
+    var popup = document.getElementById("myPopup");
+    popup.classList.toggle("show");
+    }
+
+    selectCampaing(){
+        this.router.navigate(['/campaign-detail/', this.offer.uuid]);
+    }
+
+    toggle_visibility() {
+        var popup = document.getElementById("popupBoxOnePosition");
+        popup.classList.toggle("show");
+        if(popup.style.display == "block"){
+            popup.style.display = "none";
+        }
+        else{
+            popup.style.display = "block";
+        }
     }
 
 
