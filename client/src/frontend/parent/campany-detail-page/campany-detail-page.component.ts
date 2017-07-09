63169f4ecfb40153abd73ebec80c6d4797c86080
SEBA-Dev-Station <SEBA-Dev-Station>
2017-07-09 18:21:40 +0200

    campany details

--- client/src/frontend/parent/campany-detail-page/campany-detail-page.component.ts
+++ client/src/frontend/parent/campany-detail-page/campany-detail-page.component.ts
@@ -1,21 +1,102 @@
-import {Component} from "@angular/core";
+import {Component, OnInit} from "@angular/core";
 import {CampaignService} from "../../services/offer.service";
 import {Campaign} from "../../data-objects/campaign";
-import {Observable} from "rxjs/Observable";
-
+import {isBuiltInAccessor} from "@angular/forms/src/directives/shared";
+import {Element} from "@angular/compiler";
+declare var jquery: any;
+declare var $: any;
 
 @Component ({
     selector: "app-campany-detail-page",
     templateUrl: "./campany-detail-page.component.html",
-    styleUrls: ["./campany-detail-page.component.css"]
+    styleUrls: ["./campany-detail-page.component.css"],
+    providers: [CampaignService]
 })
 
-export class CampanyDetailPageComponent {
 
+export class CampanyDetailPageComponent implements OnInit {
+    offerService: CampaignService;
+    campaign: Campaign;
+    accepted: boolean = false;
+    isActionTaken: boolean = false;
+    // buttonClicked: any;
+
+    private errorMessage: string;
+
+    constructor(offerService: CampaignService) {
+        this.offerService = offerService;
+    }
+
+
+    ngOnInit(): void {
+
+        let campaignId: string = "2";
+
+        this.offerService.getCampaignWithId(campaignId).subscribe(
+            campaign => {
+                this.campaign = campaign;
+            },
+            error => {
+                this.errorMessage = error;
+                throw new Error(error);
+            }
+        );
+
+
+        $('.acceptButton').click((event: any) => {
+            let acceptBtn = event.currentTarget;
+            let index = $(".acceptButton").index(acceptBtn);
+            let container = $("tr").get(index);
+            container.style.background = "palegreen";
+        });
+
+
+        $('.rejectButton').click((event: any) => {
+            let rejectBtn = event.currentTarget;
+            let index = $(".rejectButton").index(rejectBtn);
+            let container = $("tr").get(index);
+            container.style.background = "lightpink";
+        });
+
+        /*$('.btn').click((event:any) => {
+            let btn = event.currentTarget;
+            let index = $(".btn").index(btn);
+            console.log("Index:"+index);
+        });*/
+    }
+
+
+    public buttonClicked (event :any) {
+        /*let btn = event.currentTarget;
+        console.log("!!!!!!!!!!!!!!"+ btn);
+
+        if (btn.innerText.includes("Accept")) {
+            this.container = $(".div").get(this.index - 1);
+            this.container.style.background = "limegreen";
+        }
+        else if (btn.innerHTML.includes("Reject")) {
+            this.container = $(".div").get(this.index - 2);
+            this.container.style.background = "lightpink";
+        }*/
+    }
+
+
+    /*private actionTaken (id: string) {
+        this.buttonClicked = document.getElementById(id);
+        this.isActionTaken = true;
+
+        if (id.includes("accept")) {
+            this.accepted = true;
+        }
+        else if (id.includes("reject")) {
+            this.accepted = false;
+        }
+
+    }*/
 
    /*
 
-   static URL_ALL_OFFERS = Config.backend_address + ":" + Config.backend_port + Config.backend_base_ur + 'offers';
+
 
    constructor(private campaignService: CampaignService) {
     this.campaignService = campaignService;
