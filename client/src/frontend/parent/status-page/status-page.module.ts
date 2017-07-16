e1b312e7ba1c0a4c898598c99b6c93f570632ce5
jensKlinker <jens.klinker@peyclon.de>
2017-07-16 18:20:03 +0200

    creating status page design

--- client/src/frontend/parent/status-page/status-page.module.ts
+++ client/src/frontend/parent/status-page/status-page.module.ts
@@ -2,12 +2,14 @@ import {NgModule} from "@angular/core";
 import {StatusPageComponent} from './status-page.component';
 import {CommonModule} from "@angular/common";
 import {RecentMediaModule} from "./recent-media/recent-media.module";
+import {AppHeaderCompanyModule} from "../../app-header-company/app-header-company.module";
 import {ScoreModule} from "./score/score.module";
 
 @NgModule ({
     imports: [
         CommonModule,
         RecentMediaModule,
+        AppHeaderCompanyModule,
         ScoreModule
     ],
     declarations: [
